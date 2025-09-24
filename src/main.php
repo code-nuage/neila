b54687b0dfc1109a4a700a794e282dba165e74b6
Kman <chaltemmanuel77@gmail.com>
2025-09-24 11:38:13 +0200

    Refactor main.php to include necessary Staurie components and autoloading

--- src/main.php
+++ src/main.php
@@ -1,1 +1,28 @@
 <?php
+
+use Jugid\Staurie\Staurie;
+use Jugid\Staurie\Component\Console\Console;
+use Jugid\Staurie\Component\PrettyPrinter\PrettyPrinter;
+use Jugid\Staurie\Component\Menu\Menu;
+
+require_once __DIR__.'/vendor/autoload.php';
+
+$staurie = new Staurie('Mon premier MUD');
+
+$staurie->register([
+    Console::class,
+    PrettyPrinter::class,
+    Menu::class
+]);
+
+$container = $staurie->getContainer();
+$menu = $container->registerComponent(Menu::class);
+$menu->configuration([
+    'text' => 'Bienvenue dans mon aventure MUD !',
+    'labels' => [
+        'new_game' => 'Commencer',
+        'quit' => 'Quitter'
+    ]
+]);
+
+$staurie->run();
