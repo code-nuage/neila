eadf4151b256f0429373e70e5fb3c99151b05988
AlxsCnsl <alexis.consolo@outlook.com>
2025-09-24 10:47:16 +0200

    start:init neila project

--- REAMDE.md
+++ REAMDE.md
@@ -1,3 +1,8 @@
 # neila
 
 A [Staurie](https://github.com/JuGid/Staurie) Horror game
+
+
+composer install
+
+php src/main.php
--- src/main.php
+++ src/main.php
@@ -1,1 +1,16 @@
 <?php
+use Jugid\Staurie\Component\Console\Console;
+use Jugid\Staurie\Component\Menu\Menu;
+use Jugid\Staurie\Component\PrettyPrinter\PrettyPrinter;
+use Jugid\Staurie\Staurie;
+//use Mon\Name\Space;
+
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+$staurie = new Staurie('Neila');
+
+$staurie->register([Console::class, PrettyPrinter::class, Menu::class]);
+
+
+$staurie->run(); //LANCE LE JEU
