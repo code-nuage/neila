d2d3cd0f2898b814492744630c0b2477fd3501b7
AlxsCnsl <alexis.consolo@outlook.com>
2025-09-24 14:13:21 +0200

    feat : add cat and map

--- src/main.php
+++ src/main.php
@@ -1,16 +1,56 @@
 <?php
+
+use Jugid\Staurie\Component\Character\MainCharacter;
 use Jugid\Staurie\Component\Console\Console;
+use Jugid\Staurie\Component\Introduction\Introduction;
+use Jugid\Staurie\Component\Inventory\Inventory;
+use Jugid\Staurie\Component\Level\Level;
+use Jugid\Staurie\Component\Map\Map;
 use Jugid\Staurie\Component\Menu\Menu;
+use Jugid\Staurie\Component\Money\Money;
 use Jugid\Staurie\Component\PrettyPrinter\PrettyPrinter;
 use Jugid\Staurie\Staurie;
-//use Mon\Name\Space;
-
 
 require_once __DIR__.'/../vendor/autoload.php';
 
 $staurie = new Staurie('Neila');
 
-$staurie->register([Console::class, PrettyPrinter::class, Menu::class]);
+$staurie->register([
+    Console::class,
+    PrettyPrinter::class, 
+    MainCharacter::class, 
+    Inventory::class, 
+    Level::class
+]);
 
+$container = $staurie->getContainer();
 
-$staurie->run(); //LANCE LE JEU
+$menu = $container->registerComponent(Menu::class);
+$menu->configuration([
+    'text'=> 'Welcome a board, Capiten',
+    'labels'=> [
+        'new_game' => 'Enter the Space Ship',
+        'quit'=> 'Exit game',
+    ]
+]);
+
+$map = $container->registerComponent(Map::class);
+$map->configuration([
+    'directory'=>__DIR__.'/Maps',
+    'namespace'=>'CodeNuage\Neila\Maps',
+    'navigation'=>true,
+    'map_enable'=>true,
+    'compass_enable'=>true
+]);
+
+$introduction = $container->registerComponent(Introduction::class);
+$introduction->configuration([
+    'text'=>[
+        'This is an introduction to test the introduction component',
+        'You can use it multiline by using an array in configuration'
+    ],
+    'title'=>'Chapter 1 : The new game',
+    'scrolling'=>false
+]);
+
+$staurie->run();
